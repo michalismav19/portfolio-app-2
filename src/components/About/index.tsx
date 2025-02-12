5cb55eeb376bdadda3ab34c50cedffae499ea6e2
Michalis Mavromatakis <m.mavromatakis@flexcar.gr>
2025-02-12 19:03:38 +0200

    Fix imports for css to scss files

--- src/components/About/index.tsx
+++ src/components/About/index.tsx
@@ -1,5 +1,5 @@
 // import React from "react";
-import "./style.css";
+import "./style.scss";
 import myImage from "../../assets/profile-photo.png";
 import { GiGraduateCap } from "react-icons/gi";
 import { BsBookmarkStar } from "react-icons/bs";
@@ -7,21 +7,21 @@ import { TfiWorld } from "react-icons/tfi";
 
 const About = () => {
   return (
-    <section id="about">
+    <section id='about'>
       <h5>Get To Know</h5>
       <h2>About Me</h2>
 
-      <div className="container about__container">
-        <div className="about__me">
-          <div className="about__me-image">
-            <img src={myImage} alt="Me" />
+      <div className='container about__container'>
+        <div className='about__me'>
+          <div className='about__me-image'>
+            <img src={myImage} alt='Me' />
           </div>
         </div>
 
-        <div className="about__content">
-          <div className="about__cards">
-            <article className="about__card">
-              <GiGraduateCap className="about__icon" />
+        <div className='about__content'>
+          <div className='about__cards'>
+            <article className='about__card'>
+              <GiGraduateCap className='about__icon' />
               <h5>Degree</h5>
               <small>
                 Bsc. Department of Management Science and Technology <br />
@@ -29,14 +29,14 @@ const About = () => {
               </small>
             </article>
 
-            <article className="about__card">
-              <BsBookmarkStar className="about__icon" />
+            <article className='about__card'>
+              <BsBookmarkStar className='about__icon' />
               <h5>GPA</h5>
               <small>7.2</small>
             </article>
 
-            <article className="about__card">
-              <TfiWorld className="about__icon" />
+            <article className='about__card'>
+              <TfiWorld className='about__icon' />
               <h5>Domains</h5>
               <small>
                 <ul>
